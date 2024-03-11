52e1190e3eaef48f58f109d38c2dd535c6625cf9
Roger Thomas <rogerthomas96@outlook.com>
2024-03-11 12:09:21 -0500

    Completed part 1 of chapter exercises

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
-
+   let pageTitle = "Some Great Books";
+   let book1 = "https://i.thriftbooks.com/api/imagehandler/l/D0A1B8C00BBE8E205A957155795383C3817B5128.jpeg";
+   let book2 = "https://images.bwbcovers.com/067/The-Death-and-Life-of-Great-American-Cities-9780679741954.jpg";
+   let book3 = "https://i.thriftbooks.com/api/imagehandler/l/3634D27F68D3C35CED5093CC9B60AD8FBA183309.jpeg";
+   
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Photo of the cover of the book 'Field Guide to Urban Gardening' by Kevin Espiritu" />
+         <img src={book2} alt="Photo of the cover of the book 'The Death and Life of Great American Cities' by Jane Jacobs" />
+         <img src={book3} alt="Photo of the cover of the book 'Can I Say' by Travis Barker with Gavin Edwards" />
       </div>      
    );
 }
